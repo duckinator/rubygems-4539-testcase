adaeb913aef89499c938c0bc16e773940fe4f9a4
Ellen Marie Dash <me@duckie.co>
2021-06-03 22:56:21 -0400

    update shell scripts to work on FreeBSD.

--- bundler-2.1.4.sh
+++ bundler-2.1.4.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm -rf .bundle/ Gemfile.lock; time DEBUG_RESOLVER=1 bundle _2.1.4_ install --verbose
--- bundler-2.2.17.sh
+++ bundler-2.2.17.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm -rf .bundle/ Gemfile.lock; time DEBUG_RESOLVER=1 bundle _2.2.17_ install --verbose
